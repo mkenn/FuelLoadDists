a1bc2676fe1efee67d180ab32f827c767f8a7d27
Marion LaRocque <mtib76@uw.edu>
2017-07-04 19:43:45 -0700

    Add files via upload

--- CoverageComp.R
+++ CoverageComp.R
@@ -16,6 +16,8 @@ evt.col="LFEVTGroupCd_FINAL" #from data.file
 # results dataframe
 unique.evts<-unique(coverage.file[,cover.evt])
 datacomp<-data.frame(evt=unique.evts,dataobs=NA,cover=NA)
+start.col=3
+cur.cols=c(start.col:ncol(data.file))
 
 #looking at coverage database
 #for(i in 1:nrow(coverage.file)) # don't need to loop through the rows, and you're using i twice as an index
@@ -35,11 +37,31 @@ datacomp<-data.frame(evt=unique.evts,dataobs=NA,cover=NA)
     # third column is the total number of observations
 #    datacomp[,3]<-coverage.file[,cover.evt]
     datacomp[i,3]<-sum(coverage.file[coverage.file[,cover.evt]==unique.evts[i],"COUNT"])
-  }
-datacomp$coverProp<-datacomp$cover/sum(as.numeric(datacomp$cover))
-#}
-datacomp[datacomp$coverProp>.05&datacomp$dataobs<1000,] # open water and Agriculture
-datacomp[datacomp$coverProp>.02&datacomp$dataobs<100,] # grassland low
 
-#########
+    # fuel loading coverage tally - i have a problem with the actual input
+    # when I do portions it is coming back with the results but its not tallying-counting the observations. I think it might also be the !is.na part.
+    for(j in cur.cols)
+    {
+      datacomp[i,j+1]<-length(data.file[!is.na(data.file[,evt.col]==unique.evts[i]),"COUNT"])
+    }
+  }
+#}
+
+# determing proportion of coverage
+# this feels like it should be in the table - gives descriptive information about what the pixel coverage is for each evt and its total number of observations
+datacomp$coverProp<-datacomp$cover/sum(as.numeric(datacomp$cover))
+
+plot(datacomp$coverProp,datacomp$dataobs)
+
+# determining what the holes are, if proportion is less than a set amount and less than a set of observations
+datacomp[datacomp$coverProp > .05 & datacomp$dataobs < 1000,] # open water and Agriculture
+datacomp[datacomp$coverProp > .02 & datacomp$dataobs < 100,] # grassland low
+
+######
 # next, account for each fuel loading catgory by expanding the columns in datacomp and including a loop over the fuel category columns, adapting the code above
+
+
+
+
+
+

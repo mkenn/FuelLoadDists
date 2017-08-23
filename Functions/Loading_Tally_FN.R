35149ef94b647b3f8c6dbbe000dd40d99f52c104
Marion LaRocque <mtib76@uw.edu>
2017-08-23 09:53:41 -0700

    Add files via upload
    updated Loading tally function for all databases

--- Functions/Loading_Tally_FN.R
+++ Functions/Loading_Tally_FN.R
@@ -1,4 +1,4 @@
-# Loading Tally Function
+  # Loading Tally Function
 # The function Will determine the number of observations per loading type, ignoring the evts
 # this will help determine:
 # holes in dataset, loadings to be used in pair correlation, sensitivity analysis and distribution fitting
@@ -11,8 +11,10 @@
 # "X1000hrS_loading","X1000hrR_loading","X1000hr_loading","X10000hrS_loading","X10000hrR_loading","X10000hr_loading"
 # "GT10000hrS_loading","GT10000hrR_loading","GT10000hr_loading","cwd_sound_loading","cwd_rotten_loading","cwd_loading"
 
-loading.tally.fn<-function(data.file,start.col,cur.cols=c(start.col:ncol(data.file)),write.file=FALSE,file.name="LoadingTally.csv",show.plot=FALSE,plot.name="LoadingTallyGraph.pdf")
+loading.tally.fn<-function(data.file,start.col,write.file=FALSE,file.name="LoadingTally.csv",show.plot=FALSE,plot.name="LoadingTallyGraph.pdf")
 {
+  cur.cols=c(start.col:ncol(data.file))
+  
   tally.load<-data.frame(fueltype=names(data.file)[cur.cols],tally=NA)
   
   for(i in cur.cols)

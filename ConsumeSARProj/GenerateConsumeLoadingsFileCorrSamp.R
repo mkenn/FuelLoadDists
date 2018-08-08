843cfed4a294882f7fe163813b06497d7fe4ab89
mkenn <mkenn@uw.edu>
2018-08-08 08:31:52 -0700

    debug fofem section, change wd and avoid duplicate column names

--- ConsumeSARProj/GenerateConsumeLoadingsFileCorrSamp.R
+++ ConsumeSARProj/GenerateConsumeLoadingsFileCorrSamp.R
@@ -9,7 +9,21 @@ GenerateFuelInput.fn<-function(corr.samp.vals,nreps=NA,fbLoadNames.df,all.fbs,ba
                                     base.fofem=NA,change.units=T,mod="C",phase="F")
 {
   input.samp<-corr.samp.vals
-  fuel.matches<-match(names(corr.samp.vals),fbLoadNames.df[,3]) 
+  if(phase=="S") # then the it's possible we have aggregated CWD R and CWD S, 
+#so here to be compatible we fofem and consume we fill in the remaining size classes
+  {
+    tmp1.col<-which(names(corr.samp.vals)=="cwd_sound_loading_Mgha")
+    tmp2.col<-which(names(corr.samp.vals)=="cwd_rotten_loading_Mgha")
+    if(length(tmp1.col)>0)
+    {
+      input.samp$X1000hrS_loading_Mgha<-0
+      input.samp$GT10KhrS_loading_Mgha<-0
+      input.samp$X1000hrR_loading_Mgha<-0
+      input.samp$GT10KhrR_loading_Mgha<-0
+      
+    }
+  }
+  fuel.matches<-match(names(input.samp),fbLoadNames.df[,3]) 
   # match the database column names to rows in the matching dataframe
   # these are the columns we won't do anything with
   match.Names<-switch(mod,
@@ -24,12 +38,12 @@ GenerateFuelInput.fn<-function(corr.samp.vals,nreps=NA,fbLoadNames.df,all.fbs,ba
       {
         if(match.Names[ii]=="litter_depth"|match.Names[ii]=="duff_upper_depth"|match.Names[ii]=="DuffDepth")
         {
-          input.samp[,ii]<-corr.samp.vals[,ii]*0.393701
+          input.samp[,ii]<-input.samp[,ii]*0.393701
         }
          
          else # 1.10231 tons/Mg, 2.47105 acre/ha
         {
-          input.samp[,ii]<-corr.samp.vals[,ii]*1.10231/2.47105
+          input.samp[,ii]<-input.samp[,ii]*1.10231/2.47105
         }
       }
     }
@@ -46,22 +60,23 @@ GenerateFuelInput.fn<-function(corr.samp.vals,nreps=NA,fbLoadNames.df,all.fbs,ba
     base.loads[,match.Names]<-input.samp
     
     
-    base.loads$filename[(1:(nrow(corr.samp.vals)))]<-rep("sobolSamplefb",nrow(corr.samp.vals)) # and give each fuelbed a unique name, here, and number, below
-    base.loads$fuelbed_number[(1:(nrow(corr.samp.vals)))]<-base.fb*1000+1:nrow(corr.samp.vals)
+    base.loads$filename[(1:(nrow(input.samp)))]<-rep("sobolSamplefb",nrow(input.samp)) # and give each fuelbed a unique name, here, and number, below
+    base.loads$fuelbed_number[(1:(nrow(input.samp)))]<-base.fb*1000+1:nrow(input.samp)
   }
   if(mod=="F")
   {
+    setwd("fofem")
     cur.fofem<-base.fofem[1,]
     match2.names<-fbLoadNames.df[!is.na(fbLoadNames.df[,4]),1] # for fofem, to move between consume names and fofem names
-    match2a.names<-match2.names[!is.na(match2.names)]
+    match2a.names<-unique(match2.names[!is.na(match2.names)])
     match3.names<-fbLoadNames.df[!is.na(fbLoadNames.df[,1]),4] # for fofem, to move between consume names and fofem names
-    match3a.names<-match3.names[!is.na(match3.names)]
+    match3a.names<-unique(match3.names[!is.na(match3.names)])
     
     cur.fb<-all.fbs[all.fbs$fuelbed_number==base.fb,]
     cur.fofem[,match3a.names]<-cur.fb[,match2a.names] # replace the non-SA fuel values with those in the base fb
     base.loads<-cur.fofem[1,] # use this for the non-loading values
     
-    for(i in 2:(nrow(corr.samp.vals))) # 
+    for(i in 2:(nrow(input.samp))) # 
       base.loads<-rbind(base.loads,cur.fofem) # only rbind the necessary columns
     
     base.loads[,match.Names]<-input.samp # replace baseline fuel loading with the sampled values for the target types
